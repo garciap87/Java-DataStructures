08f9989c48b02b89b6f818830cf5e0d5dcdf6ddd
Phillip Garcia <garciap87@gmail.com>
2017-04-25 20:53:27 -0600

    Fixed bubble sort

--- DataStructures/Searching/LinearSearch.java
+++ DataStructures/Searching/LinearSearch.java
@@ -5,44 +5,44 @@ import java.util.Random;
  
 public class LinearSearch
 {
-       public static void main(String[] args)
-       {
-              Scanner scanner = new Scanner(System.in);
-              Random ran = new Random();
-              System.out.print("How many random numbers would you like to create: ");
-              int size = scanner.nextInt();
-              System.out.print("What range do you want to use: ");
-              int range = scanner.nextInt();
+   public static void main(String[] args)
+   {
+      Scanner scanner = new Scanner(System.in);
+      Random ran = new Random();
+      System.out.print("How many random numbers would you like to create: ");
+      int size = scanner.nextInt();
+      System.out.print("What range do you want to use: ");
+      int range = scanner.nextInt();
  
-              int[] array = new int[size];
+      int[] array = new int[size];
              
-              for(int i =0; i < array.length; i++) 
-              {
-                     int random = ran.nextInt(range) + 1;
-                     array[i] = random;
-              }
+      for(int i =0; i < array.length; i++) 
+      {
+         int random = ran.nextInt(range) + 1;
+         array[i] = random;
+      }
              
-              System.out.print("What number do you want to search for: ");
-              int search = scanner.nextInt();
-             
-              linearSearch(array,search);
-              scanner.close();
-       }
-      
-       public static void linearSearch(int x[], int value)
-       {
-              boolean found = false;
-             
-              for(int i = 0; i < x.length && !found; i++)
-              {
-                     if(x[i] == value)
-                     {
-                           found = true;
-                           System.out.println("The value was found! At position " + i);
-                     }
-              }
-             
-              if(!found)
-                     System.out.println("The value was not found!");
-       }
+      System.out.print("What number do you want to search for: ");
+      int search = scanner.nextInt();
+     
+      linearSearch(array,search);
+      scanner.close();
+   }
+  
+   public static void linearSearch(int x[], int value)
+   {
+      boolean found = false;
+     
+      for(int i = 0; i < x.length && !found; i++)
+      {
+         if(x[i] == value)
+         {
+               found = true;
+               System.out.println("The value was found! At position " + i);
+         }
+      }
+     
+      if(!found)
+    	  System.out.println("The value was not found!");
+   }
 }
--- DataStructures/Sorting/BubbleSort.java
+++ DataStructures/Sorting/BubbleSort.java
@@ -4,46 +4,51 @@ import java.util.Random;
  
 public class BubbleSort
 {
-       public static void main(String[] args)
-       {
-              Scanner scanner = new Scanner(System.in);
-              Random ran = new Random();
-              System.out.print("How many random numbers would you like to create: ");
-              int size = scanner.nextInt();
-              System.out.print("What range do you want to use: ");
-              int range = scanner.nextInt();
- 
-              int[] array = new int[size];
-             
-              for(int i =0; i < array.length; i++) 
-              {
-                     int random = ran.nextInt(range) + 1;
-                     array[i] = random;
-              }
-             
-              System.out.print("What number do you want to search for: ");
-             
-              bubbleSort(array);
-              System.out.print("The array in ascending order is: " + array);
-              scanner.close();
-       }
+   public static void main(String[] args)
+   {
+      Scanner scanner = new Scanner(System.in);
+      Random ran = new Random();
+      System.out.print("How many random numbers would you like to create: ");
+      int size = scanner.nextInt();
       
-       public static void bubbleSort(int x[])
+      System.out.print("What range do you want to use: ");
+      int range = scanner.nextInt();
+ 
+       int[] array = new int[size];
+             
+       for(int i =0; i < array.length; i++) 
        {
-              boolean swap;
-              do
-              {
-                     swap = false;
-                     for(int i =0; i < x.length-1; i++)
-                     {
-                           if(x[i] > x[i+1])
-                           {
-                                  int temp = x[i];
-                                  x[i] = x[i+1];
-                                  x[i+1] = temp;
-                                  swap = true;
-                           }
-                     }
-              }while(swap);
+          int random = ran.nextInt(range) + 1;
+          array[i] = random;
        }
+             
+       bubbleSort(array);
+          
+      System.out.print("The array in ascending order is: [");
+      for(int j =0; j < array.length; j++) 
+      {
+    	  System.out.print(array[j] + ", ");
+      }
+      System.out.print("]");
+      scanner.close();
+   }
+  
+   public static void bubbleSort(int x[])
+   {
+	  boolean swap;
+      do
+      {
+    	  swap = false;
+          for(int i =0; i < x.length-1; i++)
+          {
+        	  if(x[i] > x[i+1])
+              {
+                int temp = x[i];
+                x[i] = x[i+1];
+                x[i+1] = temp;
+                swap = true;
+              }
+         }
+      }while(swap);
+   }
 }
